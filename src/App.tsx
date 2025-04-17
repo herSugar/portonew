670e1e815e5ceed11017cc225907083dbcc0a12f
herSugar <thereaperx008@gmail.com>
2025-04-17 08:44:23 +0800

    update2

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 // App.tsx
-import Navbar from "./components/navbar";
+import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
